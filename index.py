32047b364ec12150a0dba69fdb7884bef9ca1fb0
Ox6A <120139450+Ox6A@users.noreply.github.com>
2024-08-11 18:13:08 +0100

    final commit v1

--- index.py
+++ index.py
@@ -7,6 +7,7 @@ import time
 from discord import app_commands
 
 config = {}
+debugMode = False
 
 logo = '''
                           ####+
@@ -31,8 +32,6 @@ logo = '''
     +###                                        +###########+
   +#+                                               #####++
                                                        ++                           '''
-
-PDActivityChannel = 1269777958860492883
 
 token = None
 embed1 = None
@@ -85,17 +84,40 @@ async def fetchActivity(channel, steamID):
 @client.tree.command(name="activity", description="Fetch activity for a SteamID in the past week")
 async def activity(interaction: discord.Interaction, steamid: str):
     global embed1
-    global PDActivityChannel
     channelToBeUsed = None
+    guildToBeUsed = None
+    inEnabledGuild = False
+    debuggingUserAllowed = False
 
-    PDChannelObj = client.get_channel(PDActivityChannel)
-    if PDChannelObj:
+    print(f"{datetime.now()} - {interaction.user.name} has searched the activity for {steamid}")
+
+    if debugMode == True:
+        for i in config["debug_users"]:
+            if i == interaction.user.id:
+                debuggingUserAllowed = True
+        if debuggingUserAllowed == False:
+            embed1 = discord.Embed(title="Debugging Mode", description="The bot is currently in a debuggging mode for testing or maintenance. Sorry for the inconvenience!", color=0xFF0000)
+            await interaction.response.send_message(embed=embed1, ephemeral=True)
+            return
+
+    for i in config["guilds"]:
+        if config["guilds"][i] == interaction.guild.id:
+            for i1 in config["enabled_dept"]:
+                if i1 == i:
+                    guildToBeUsed = config["guilds"][i]
+                    guildName = i
+                    inEnabledGuild = True
+    if inEnabledGuild == False:
+        print("\033[93m\033[1mWARNING: Command sent in a disallowed guild\033[0m")
+        return
+    ChannelObj = client.get_channel(config["channelid"][guildName])
+    if ChannelObj:
         try:
             caller = interaction.guild.get_member(interaction.user.id)
             if caller:
-                permissions = PDChannelObj.permissions_for(caller)
+                permissions = ChannelObj.permissions_for(caller)
                 if permissions.read_messages:
-                    channelToBeUsed = PDChannelObj
+                    channelToBeUsed = ChannelObj
                 else:
                     embed1 = discord.Embed(title="Permission Denied", description="You do not have the required permsisions to use this bot. If this in error, please contact `teasippingbrit` on Discord.", color=0xFF0000)
                     await interaction.response.send_message(embed=embed1, ephemeral=True)
@@ -108,6 +130,9 @@ async def activity(interaction: discord.Interaction, steamid: str):
                 f.write(e)
             await sendErrorMsg(interaction)
             return
+    else:
+        embed1 = discord.Embed(title="Channel not found", description="The logging channel was not found. This is likely a configuration error, or the result of changes to the logging channels. Please contact `teasippingbrit` on Discord.", color=0xFF0000)
+        await interaction.response.send_message(embed=embed1, ephemeral=True)
 
 
     if channelToBeUsed == None:
@@ -141,7 +166,7 @@ def loadConfig():
         print("\033[1mGenerating default configuration...\033[0m")
         try:
             with open("config.json", "w") as f:
-                config = {"channelid":{"pd":1090365529564385300,"rpu":1141061367730806900,"sco":1081600560060444900,"nca":1079508453241917700,"nca_undercover":1158378499313246200,"nhs":1117803842646585500,"rs":1100029126758375400,"t":1210141399878738000},"enabled_discords":{"pd":472520717515096060},"minimum_role_requirement":{"pd":1179100367083016200,"rpu":1141037906467766400,"sco":811699338253172700,"nca":1241570118014599200,"nca_undercover":0,"nhs":752960738544451700,"rs":752961421284868100,"t":752961594194919400},"debug":False}
+                config = {"channelid":{"pd":1090365529564385310,"rpu":1141061367730806906,"sco":1081600560060444885,"nca":1079508453241917653,"nca_undercover":1158378499313246248,"nhs":1117803842646585424,"rs":1100029126758375485,"t":1210141399878737920,"test":1269777958860492883},"guilds":{"pd":472520717515096078,"rpu":472520717515096078,"sco":472534240576143401,"nca":473075559409385472,"nca_undercover":473075559409385472,"nhs":472537475605069825,"rs":472897608759771146,"t":472715289516048385,"test":1264515893610680393},"enabled_dept":["test","pd"],"minimum_role_requirement":{"pd":1179100367083016233,"rpu":0,"sco":811699338253172736,"nca":1241570118014599278,"nca_undercover":0,"nhs":752960738544451715,"rs":752961417606332506,"t":752961594194919478,"test":1272168995419717713},"debug_users":["743066712810717295"],"debug":False}
                 json.dump(config, f)
             return True
         except Exception as e1:
@@ -151,6 +176,7 @@ def loadConfig():
 
 def main():
     global token
+    global debugMode
     print("\n\033[1m\033[94mRiverside Activity Calculator Bot: Starting...\033[0m\n" + logo)
     loadTokenResult = loadToken()
     if not loadTokenResult:
@@ -161,6 +187,7 @@ def main():
         return
     print("\033[1mSuccessfully loaded configuration!\033[0m\n")
     if config["debug"] == True:
+        debugMode = True
         print("\033[1m\033[91mWARNING: Running in debug mode!\033[0m")
     print("\033[1mStarting bot...\033[0m")
     client.run(token)
